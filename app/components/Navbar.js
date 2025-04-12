d7e442f5da2a90c13a307903174f38f26d3738ec
Abd-baap <abhigyaworkspace@gmail.com>
2025-04-12 19:44:47 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,4 +1,6 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,13 +13,13 @@ export default function Navbar() {
     return (
       <div className="navbar flex p-1 px-4 text-[24px] sticky border w-screen h-min rounded-lg justify-between hover:shadow-md">
         <div className="logo flex gap-2 items-center">
-          <a className="flex gap-1" href="/">
+          <Link className="flex gap-1" href="/">
             <Image alt="Logo" loading="lazy" width="40" height="40" src="/logo.gif" />
             <div className="text">
               <span className="font-bold">Goal</span>
               <span className="font-bold text-red-800">Go</span>
             </div>
-          </a>
+          </Link>
         </div>
         <DropdownMenu>
       <DropdownMenuTrigger><Image src="/list.png" width={35} height={35}alt='index image'></Image></DropdownMenuTrigger>
